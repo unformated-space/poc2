edefa4a43b170f1f67de0bc528e50b73c9d2893d
Namen Malkav <namenmalkav@gmail.com>
2024-04-28 21:18:44 -0300

    "Chuck Norris is watching you as you read this joke. If you want it to end quickly, don't turn around."

--- scripts/motionController.gd
+++ scripts/motionController.gd
@@ -1,23 +1,19 @@
-# InputManager.gd
 extends RigidBody3D
 
-@export var  inVehicle := false
+@export var inVehicle := false
 var twist_input := 0.0
 var pitch_input := 0.0
 
-#func _ready():
-	#lock_rotation = true
-
-	
 @onready var camera_controller = get_node("./CameraController")
-var move_input :=Vector3.ZERO
+var move_input := Vector3.ZERO
 var velocity := Vector3.ZERO
 var acceleration = 0.5
 var accel_multiplier = 3.0
 var max_speed = 12.0
 var sprint_speed = 12.0
 var normal_speed = 7.0
-# Handle input events
+var align_to_gravity = false
+var gravity_align_speed = 2.0 
 
 var jetpack := false
 var dampeners := true
@@ -28,32 +24,48 @@ var jumping := false
 @onready var speedLabel = get_node("../../GUI/speed")
 @onready var dampenerLabel = get_node("../../GUI/dampeners")
 @onready var jetpackLabel = get_node("../../GUI/jetpack")
-func _ready():
-	text.scroll_following=true
-	lock_rotation=true
-	# Damping can be set to simulate air resistance, if needed
 
-	
-	# Other properties you might want to configure
-	#friction = 1.0  # Adjust friction to control sliding behavior
-	#bounce = 0.0  # No bounce; adjust as necessary for your simulation needs
+func _ready():
+	text.scroll_following = true
+	lock_rotation = true
 
 func is_on_floor() -> bool:
 	return $RayCast3D.is_colliding()
-
 	
+func can_move() -> bool:
+	# Definir las condiciones bajo las cuales el movimiento está permitido o bloqueado
+	return is_on_floor() or jetpack
+
+func maintain_current_state():
+	# Aquí puedes definir lo que sucede cuando los inputs están bloqueados
+	# Por ejemplo, mantener la velocidad actual o aplicar una fuerza de gravedad si está en el aire
+	
+	if not is_on_floor() and not jetpack:
+		apply_central_force(Vector3(0, 1 * mass, 0))  # Asegurando que la gravedad sigue aplicándose
+
+
 func _process(delta):
-	#text.append_text(str(camera_controller.cameraAngle)+"\n")
 	var speed_m_s = linear_velocity.length()  # This is the speed in meters per second
-	speedLabel.text="Speed: " + str(speed_m_s) + " m/s"
-	dampenerLabel.text="Dampeners "+str(dampeners)
-	jetpackLabel.text = "jetpack "+str(jetpack)
-	pass
+	speedLabel.text = "Speed: " + str(speed_m_s) + " m/s"
+	dampenerLabel.text = "Dampeners " + str(dampeners)
+	jetpackLabel.text = "Jetpack " + str(jetpack)
+	text.append_text(str(is_on_floor())+"\n")
+
+
 func _physics_process(delta):
 	rotate_y(camera_controller.cameraAngle)
 	camera_controller.cameraAngle = 0.0
+	if can_move():  # Comprobar si el jugador puede moverse
+		var direction = handle_movement_input(Vector3.ZERO, delta)
+		var speed = handle_speed_input()
+		apply_movement(direction, speed, delta)
+	else:
+		maintain_current_state()
+		align_to_gravity_direction(delta)
 
-	var direction = Vector3.ZERO
+
+
+func handle_movement_input(direction, delta):
 	if Input.is_action_pressed("move_fwd"):
 		direction += -transform.basis.z
 	if Input.is_action_pressed("move_back"):
@@ -62,66 +74,59 @@ func _physics_process(delta):
 		direction += -transform.basis.x
 	if Input.is_action_pressed("move_right"):
 		direction += transform.basis.x
-	
 	if jetpack:
 		if Input.is_action_pressed("jump"):
 			direction += transform.basis.y
 		if Input.is_action_pressed("crouch"):
 			direction += -transform.basis.y
-		if Input.is_action_pressed("rotate_left"):
-			rotate_z( deg_to_rad(lerp( position.z ,position.z, delta * Config.lerpSpeed)))
-		if Input.is_action_pressed("rotate_right"):
-			rotate_z( -deg_to_rad(lerp( position.z ,position.z, delta * Config.lerpSpeed)))
 		rotate_x(camera_controller.cameraAngleZ)
-		camera_controller.cameraAngleZ = 0.0
+	return direction
 
-	
-	# TODO: add crouching
-	# TODO: fix jump
-	if Input.is_action_pressed("jump") && !jumping:
-		direction.y += 50
-		jumping = true
-	
-	
-	var speed = 0.0
-	if Input.is_action_pressed("sprint-boost"):
-		speed = sprint_speed
-	else:
-		speed = normal_speed
-		
+func handle_speed_input():
 	if Input.is_action_just_released("jetpack"):
 		jetpack = !jetpack
-
 	if Input.is_action_just_released("inertiaDampener"):
 		dampeners = !dampeners
 
 	if jetpack:
-		speed =20
 		if dampeners:
 			linear_damp = 5
 		else:
 			linear_damp = 1
 		gravity_scale = 0
+		return 20
 	else:
-		rotation.z = lerp( rotation.z ,0.0, delta * Config.lerpSpeed)
-		rotation.x = lerp( rotation.x ,0.0, delta * Config.lerpSpeed)
-		
+		align_to_gravity = true
 		gravity_scale = 1
 		linear_damp = 0
-	#text.append_text(str(linear_damp)+"\n")
-	#
-	
-	#if direction.length() > 0:
-		#direction = direction.normalized() * speed
+		if Input.is_action_pressed("sprint-boost"):
+			return sprint_speed
+		else:
+			return normal_speed
 
 
-	apply_central_force(direction.normalized()*100)
-	# Obtener la velocidad actual del RigidBody
+func apply_movement(direction, speed, delta):
+	if direction.length() > 0:
+		direction = direction.normalized() * speed
+	apply_central_force(direction * 100)
+
 	var current_velocity = linear_velocity
 	var current_speed = current_velocity.length()
-
-	# Comprobar si la velocidad excede el máximo permitido
 	if current_speed > speed:
-		# Ajustar la velocidad para mantenerla dentro del límite máximo
 		current_velocity = current_velocity.normalized() * speed
 		linear_velocity = current_velocity
+	if jetpack:
+		if Input.is_action_pressed("rotate_left"):
+			rotate_z(deg_to_rad(-1))  # Rotar a una velocidad fija por frame
+		if Input.is_action_pressed("rotate_right"):
+			rotate_z(deg_to_rad(1))  # Rotar a una velocidad fija por frame
+
+
+
+
+func align_to_gravity_direction(delta):
+	rotation.z = lerp( rotation.z ,0.0, delta * Config.lerpSpeed)
+	rotation.x = lerp( rotation.x ,0.0, delta * Config.lerpSpeed)
+
+
+
