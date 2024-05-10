266329b85a40503629a2ed24ca9e841e717caf4b
Namen Malkav <namenmalkav@gmail.com>
2024-05-10 09:59:47 -0300

    "Chuck Norris will find you if you don't like this joke."

--- grid_system2/Block2.gd
+++ grid_system2/Block2.gd
@@ -27,24 +27,24 @@ func _ready():
 		var grid_area = get_node("/root/world/_grids/"+UUID+"/grid_area")
 
 
-		#var area_mesh = MeshInstance3D.new()
-		#area_mesh.mesh = mesh.mesh
-		#var area_collision_shape = CollisionShape3D.new()
-		#area_collision_shape.set_shape (mesh.mesh.create_convex_shape())
-		#area_collision_shape.name = "area_body_collision"
-		#area_mesh.mesh = mesh.mesh
-		#area_mesh.set_layer_mask_value(1, false)
-		#area_mesh.set_layer_mask_value(2, true)
-		#area_mesh.name= "area_body_mesh"
-#
-		#grid_area.add_child(area_collision_shape)
-		#grid_area.add_child(area_mesh)
-		#area_mesh.global_transform = global_transform
-		#grid_area.global_transform = global_transform
+		var area_mesh = MeshInstance3D.new()
+		area_mesh.mesh = mesh.mesh
+		var area_collision_shape = CollisionShape3D.new()
+		area_collision_shape.set_shape (mesh.mesh.create_convex_shape())
+		area_collision_shape.name = "area_body_collision"
+		area_mesh.mesh = mesh.mesh
+		area_mesh.set_layer_mask_value(1, false)
+		area_mesh.set_layer_mask_value(2, true)
+		area_mesh.name= "area_body_mesh"
+#_mesh"
+		grid_area.add_child(area_collision_shape)
+		grid_area.add_child(area_mesh)
+		area_mesh.global_transform = global_transform
+		grid_area.global_transform = global_transform
 
-		#print (grid_area.get_children())
-		#print(grid_area.name)
-		#grid_area.global_transform = global_transform
+		print (grid_area.get_children())
+		print(grid_area.name)
+		grid_area.global_transform = global_transform
 		
 		#collision adding 
 		var grid_mesh = MeshInstance3D.new()
@@ -81,43 +81,49 @@ func add_block(hit_normal,object, interact=true):
 	print ("entre aca")
 	debugger ([parent.name, parent.get_class()])
 	var grid_size =Vector3(0.5, 0.5, 0.5)
-	#var grid_size =Vector3(1, 1, 1)w
+
 	# Compute the grid position based on block size
 	var grid_position = (object.transform.origin )+( hit_normal* grid_size ) 
 	#debugger([object.transform.origin.floor() , hit_normal.floor()])
 	# Check if there is already a block at this position (optional, depending on your design)
 	for child in grid_container.get_children():
-		if child.global_transform.origin == grid_position:
+		if child.global_transform.origin == grid_position&& child.name.begins_with("area_from"):
 			return # Block already exists at this positiondw
-
+	#var grid_position =(object.transform.origin.snapped(grid_size))+( hit_normal.snapped(grid_size)* grid_size )
+	## Check if there is already a block at this position (optional, depending on your design)
+	#if object.transform.origin.snapped(grid_size) == hit_normal.snapped(grid_size):
+		#print ("son igaules")
+	#debugger ([grid_position, object.transform.origin, hit_normal])
+	#debugger ([grid_position, object.transform.origin.snapped(grid_size), hit_normal.snapped(grid_size)])
+	#for child in grid_container.get_children():
+		#if child.name.begins_with("area_from_"):
+			#print (child.transform.origin.snapped(grid_size))
+			#grid_position =(child.transform.origin.snapped(grid_size))+( hit_normal.snapped(grid_size)* grid_size )
+			#debugger ([grid_position, child.transform.origin.snapped(grid_size), hit_normal.snapped(grid_size)])
+			#if (object.transform.origin.snapped(grid_size))+( hit_normal.snapped(grid_size)* grid_size ) == grid_position:
+				#print ("block_already in position")
+				#return # Block already exists at this positiondw
+			#elif child.transform.origin.snapped(grid_size) == grid_position:
+				#print ("block_already in position")
+				#return # Block already exists at this positiondw
 	# Instance the block
 	var block_instance = load(BASE_STATIC).instantiate()
-
-	# Add the block instance to the current node or another parent node
-
 	block_instance.transform.origin = grid_position
 	var block_collmesh = block_instance.get_node("collision")
 	var block_mesh = block_instance.get_node("mesh")
-
-	block_collmesh.name = "collision_from_"+str(grid_position).replace(" ","")
-	block_instance.name = "area_from_"+str(grid_position).replace(" ","")
+	var grid_coll_mesh=block_collmesh.duplicate()
+	block_collmesh.name = "collision_from_"+str(grid_position)
+	block_instance.name = "area_from_"+str(grid_position)
 	#random color
 	var material = StandardMaterial3D.new()
 	material.albedo_color = random_color()
 	block_mesh.material_override = material
 	
-
 	
 	var grid_body = get_node(str(parent.get_path())+"/grid_body")
-	grid_container.add_child(block_instance)
-	
-	var new_collision_for_body = CollisionShape3D.new()
-	new_collision_for_body.set_shape (get_node(block_collmesh.get_path()).get_shape())
-	new_collision_for_body.name = block_collmesh.name
-	grid_body.add_child(new_collision_for_body)
-	new_collision_for_body.global_transform = global_transform
-	
-	
+	grid_body.add_child(block_instance)
+	grid_coll_mesh.reparent(grid_container, true)
+
 ## TODO: on remove block si se remueve 0.0.0 el rigid body se tiene q mover lo mejor seria q el rigid body no tenga volumen y se mueva siempre al centro de maza
 func random_color():
 	# Genera valores aleatorios para los componentes rojo, verde y azul
